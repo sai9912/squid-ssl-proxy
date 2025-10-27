a1cb29a8d560337e8d3a4e02e32dbada03361351
sai9912 <39256353+sai9912@users.noreply.github.com>
2025-10-27 18:28:09 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt update -y
+apt install docker.io -y
+sudo rm /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
