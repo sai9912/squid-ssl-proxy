3b27a31331f7ae3c1d85d20226838d3fc17dfd64
sai9912 <39256353+sai9912@users.noreply.github.com>
2025-10-27 19:39:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt update -y
 apt install docker.io -y
 sudo rm /usr/local/bin/docker-compose
@@ -5,3 +7,7 @@ sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.2/docker
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+sed -i -E 's|(http_port )[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(:8000 .* name=ip1)|\145.120.138.113\2|; s|(tcp_outgoing_address )[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+( to_ip1)|\145.120.138.113\2|' ./squid.conf
+
